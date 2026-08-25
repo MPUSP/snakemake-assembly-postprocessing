b8b013b21cd05c44b64c2912305e167564aff176
m-jahn <jahn@mpusp.mpg.de>
2026-08-25 08:54:20 +0200

    fix: formatting, early-fail on empty df, type enforcement for sample sheet

--- workflow/scripts/prepare_names.py
+++ workflow/scripts/prepare_names.py
@@ -15,28 +15,35 @@ ref = snakemake.params["ref"]
 
 # read sample sheet
 try:
-    df_samples = pd.read_csv(sample_sheet)
+    df_samples = pd.read_csv(sample_sheet, dtype=str)
     sys.stderr.write(f"Read sample sheet from {snakemake.params['sample_sheet']}\n")
 except Exception as e:
-    sys.stderr.write(
-        f"Error reading sample sheet from {snakemake.params['sample_sheet']}: {e}\n"
-    )
+    raise RuntimeError(
+        f"Failed to read sample sheet from {snakemake.params['sample_sheet']}: {e}"
+    ) from e
 
 df_samples["file"] = df_samples["file"].apply(lambda x: os.path.basename(x))
 
 # add reference if provided
 if ref:
     ref_file, ref_name = ref
-    df_ref = pd.DataFrame({"file": [os.path.basename(ref_file)], "sample": [ref_name] if ref_name else [os.path.basename(ref_file)]})
+    df_ref = pd.DataFrame(
+        {
+            "file": [os.path.basename(ref_file)],
+            "sample": [ref_name] if ref_name else [os.path.basename(ref_file)],
+        }
+    )
     df_samples = pd.concat([df_samples, df_ref], ignore_index=True)
 
 # ntSynt mutates "_" to " "
-df_samples["sample"] = df_samples["sample"].apply(lambda x: x.replace("_", "-").replace(" ", "_"))
+df_samples["sample"] = df_samples["sample"].apply(
+    lambda x: x.replace("_", "-").replace(" ", "_")
+)
 
 try:
     df_samples[["file", "sample"]].to_csv(outfile, sep="\t", index=False, header=False)
     sys.stderr.write(f"Wrote ntSynt-viz sample name mapping to {outfile}\n")
 except Exception as e:
-    sys.stderr.write(
-        f"Error writing ntSynt-viz sample name mapping to {outfile}: {e}\n"
-    )
+    raise RuntimeError(
+        f"Failed to write ntSynt-viz sample name mapping to {outfile}: {e}"
+    ) from e
